bbd9f3aa356ae2cf6068ccd8bdbec74538214dbf
GiovanniUrso <121301413+GiovanniUrso@users.noreply.github.com>
2025-10-29 16:32:26 +0100

    .

---
+++ CSS/Mediaquery/JS/script.js
@@ -0,0 +1,11 @@
+function getInfo(){
+
+
+    let name = document.getElementById("nome");
+    let email = document.getElementById("email");
+    let message = document.getElementById("cognome"); 
+    name.textContent = "simone";
+    message.textContent = "bianchi";
+    email.textContent = "G2PfO@example.com";  
+    console.log(name, email, message);
+}
